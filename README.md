17ca11ce4a0d65d2ae752ec63f79522d0ae46492
Nirmal Gupta <nirmalk.gupta@gmail.com>
2015-07-20 11:01:40 +0530

    updated.

--- README.md
+++ README.md
@@ -4,6 +4,9 @@ making async code practically synchronous
 - for simple callback using galaxy   
   ```node --harmony with_galaxy.js```  
 - promise with q  
-```node --harmony with_q.js```  
+  ```node --harmony with_q.js```  
 - promise with bluebird  
-```node --harmony with_bluebird.js```
+  ```node --harmony with_bluebird.js```
+- running generator functions in parallel   
+  ```node --harmony allInParallel.js```
+  
---
+++ allInParallel.js
@@ -0,0 +1,41 @@
+'use strict';
+
+var bluebird = require('bluebird');
+var util = require('util');
+var assert = require('assert');
+
+/**
+ * typical callback
+ */
+function calculatorCB(input, outputCallback) {
+  util.log('calP', input);
+  setTimeout(function () {
+    util.log('calP done', input);
+    outputCallback(undefined, input * 10);
+  }, 1000)
+}
+
+/**
+ * promisify typical callback function.
+ */
+var calP = bluebird.promisify(calculatorCB);
+
+function* calc(input) {
+  return (yield calP(input))
+}
+
+var test = bluebird.coroutine(function* () {
+  var result1  = yield* runParallel(calc.bind(undefined, 1), calc.bind(undefined, 2));
+  util.log(result1);
+});
+
+function* runParallel(){
+  var promises = [];
+  for(var i = 0; i < arguments.length; i++){
+    promises.push(bluebird.coroutine(arguments[i])())
+  }
+  return yield bluebird.all(promises);
+}
+
+test();
+
